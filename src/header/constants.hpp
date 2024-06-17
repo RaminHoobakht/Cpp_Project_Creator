20a2ec6486814662dc9083da5014d6f809e89db9
ramin <ir.linux.kernel.developer@gmail.com>
2024-06-17 22:15:30 +0330

    after adding rt variable

--- src/header/constants.hpp
+++ src/header/constants.hpp
@@ -42,7 +42,7 @@ namespace constants {
     static constexpr std::string error_title{" Error: "};
 
     static const std::string title{
-            " Ramin Hoobakht, C++ project creator v.0.1.27 2024 May 29"};
+            " Ramin Hoobakht, C++ project creator v.0.1.28 2024 Jun 17"};
 
     static const std::string param_error_one{" invalid parameter ..."};
 
